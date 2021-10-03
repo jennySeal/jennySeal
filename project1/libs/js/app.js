872ac50ebc6644029f8ee57209b2be6b6bd11de9
Jenny Seal <jennysealft@gmail.com>
2021-10-03 12:05:22 +0100

    Restyled as open close modal

--- project1/libs/js/app.js
+++ project1/libs/js/app.js
@@ -1,9 +1,21 @@
 import {jawgKey} from './config.js';
+
+//Set up country object with all the info options
+let country = {
+	iso2: "", 
+	population: 0,
+	countryName: "", 
+	currency: "",
+	capital: "",
+	flag: "",
+	area: 0
+} 
+
 //Set up Leaflet maps
 let map = L.map('map').fitWorld();
 
 //using Jawg Streets
-let mapDesign = L.tileLayer('https://{s}.tile.jawg.io/jawg-streets/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
+let mapDesign = L.tileLayer('https://{s}.tile.jawg.io/jawg-sunny/{z}/{x}/{y}{r}.png?access-token={accessToken}', {
 	attribution: '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	minZoom: 0,
 	maxZoom: 22,
@@ -13,12 +25,12 @@ let mapDesign = L.tileLayer('https://{s}.tile.jawg.io/jawg-streets/{z}/{x}/{y}{r
 });
 
 mapDesign.addTo(map);
-map.locate({setView: true, maxZoom: 5});
+map.locate({setView: true, maxZoom: 4});
 
+//On load find the user's location
 const onLocationFound = (e) => {
     let radius = e.accuracy;
      L.circle(e.latlng, radius).addTo(map);
-	 console.log(e.latlng.lat, e.latlng.lng);
 	 getCountryCode(e.latlng.lat, e.latlng.lng);
 	 }
 
@@ -28,14 +40,65 @@ function onLocationError(e) {
 }
 map.on('locationerror', onLocationError);
 
+// Set up buttons to open and close modal
+$('#closeModal').click(function(){
+	$('#modal').hide()
+	});
+
+$('#homebutton').click(function(){
+	$('#modal').show()
+});
+
+
+// ************************START HERE *************************
+
+$('#weatherIcon').click(function(){
+	callApi('getWeatherInfo', 'en', country.iso2, writeTopLevel);
+	$('#modal').show()
+});
+
+$('#virusIcon').click(function(){
+	$('#modal').show()
+});
+
+$('#moneyIcon').click(function(){
+	$('#modal').show()
+});
+
+$('#transportIcon').click(function(){
+	$('#modal').show()
+});
+
 //Once we have coordinates we can get the country code
 const getCountryCode = (lat, lng) => {
 	callApi('getCountryCode', lat, lng, useCountryCode)
 	}
-// and use it to get top level info
+// and use it to get the name, population, capital, area, currency and flag info
 const useCountryCode = (data) => {
-	console.log(data.data)
+	country.iso2 = data.data;
+	callApi('getCountryInfo', 'en', country.iso2, writeTopLevel);
 }
+
+const writeTopLevel = (data) => {
+	let results = data.data[0]
+	country.population = parseFloat(results.population / 1000000);
+	country.countryName = results.countryName;
+	country.currency = results.currencyCode;
+	country.capital = results.capital;
+	country.flag = `https://www.countryflags.io/${country.iso2}/shiny/32.png`;
+	country.area = results.areaInSqKm;
+
+	$('#titleCountry').html(country.countryName);
+	$('#capital').html(country.capital);
+	$('#population').html(country.population.toFixed(2));
+	$('#currency').html(country.currency);
+	$('#flag').attr("src", country.flag);
+	$('#area').html(country.area);
+}
+
+// once the weather button is clicked get the weather info
+
+
 
 
 //Generic function for API call 
@@ -46,8 +109,8 @@ const callApi = (phpToCall, parameter1, parameter2, callbackFun) => {
         type: 'POST',
         dataType: 'json',
         data: {
-            lat: parameter1,
-            lng: parameter2,
+            param1: parameter1,
+            param2: parameter2,
 		}, 
 		success: function(result) {
 			callbackFun(result)
--- project1/libs/php/getCountryInfo.php
+++ project1/libs/php/getCountryInfo.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 
 $executionStartTime = microtime(true);
 
-$url='http://api.geonames.org/countryCode?lat=' . $_REQUEST['param1'] . '&lng=' . $_REQUEST['param2'] . '&username=' . $geonameKey;
+$url='http://api.geonames.org/countryInfoJSON?formatted=true&lang=' . $_REQUEST['param1'] . '&country=' . $_REQUEST['param2'] . '&username=' . $geonameKey .'&style=full';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -22,10 +22,11 @@ $output['status']['code'] = "200";
 $output['status']['name'] = "ok";
 $output['status']['description'] = "success";
 $output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-$output['data'] = trim($result);
+$output['data'] = $decode['geonames'];
 
-print_r(json_encode($output));
 
 header('Content-Type: application/json; charset=UTF-8');
+
+echo json_encode($output);
 
 ?>
