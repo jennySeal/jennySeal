f0d1f3a385f65464d68e24627e46167ee91988da
Jenny Seal <jennysealft@gmail.com>
2021-11-04 14:01:56 +0000

    order bys added to php gets

--- project2/libs/php/getAllDepartments.php
+++ project2/libs/php/getAllDepartments.php
@@ -26,7 +26,7 @@
 
 	// SQL does not accept parameters and so is not prepared
 
-	$query = 'SELECT id, name, locationID FROM department';
+	$query = 'SELECT id, name, locationID FROM department ORDER BY name';
 
 	$result = $conn->query($query);
 	
