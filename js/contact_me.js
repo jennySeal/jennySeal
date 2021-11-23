a071a33afb22d1a74a8b005131d07c50fe88f300
Jenny Seal <jennysealft@gmail.com>
2021-11-23 09:44:20 +0000

    Changes to the contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,8 +1,3 @@
-/*
-  Jquery Validation using jqBootstrapValidation
-   example is taken from jqBootstrapValidation docs 
-  */
-
 
 $(function() {
 
@@ -56,7 +51,7 @@ $(function() {
  		 $('#success').html("<div class='alert alert-danger'>");
             	$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             	 .append( "</button>");
-            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:me@example.com?Subject=Message_Me from myprogrammingblog.com'>me@example.com</a> ? Sorry for the inconvenience!");
+            	$('#success > .alert-danger').append("<strong>Sorry "+firstName+" it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='mailto:jennysealft@gmail.com?Subject=Message_Me from jennyseal.com'>jennysealft@gmail.com</a> ? Sorry for the inconvenience!");
  	        $('#success > .alert-danger').append('</div>');
  		//clear all fields
  		$('#contactForm').trigger("reset");
